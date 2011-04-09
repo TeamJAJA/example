3bbe3558556f4d81445d01fbacbbaafa950ff64c
TeamJAJA <jimxie@ucdavis.edu>
2011-04-09 15:25:07 -0700

    Cannot unstage debug line. Fatal: corrupt patch at line 23

--- testing.c
+++ testing.c
@@ -3,5 +3,15 @@
 int main(int argc, char **argv)
 {
 	printf("TESTING!\n");
+	
+	char name[255];
+
+	printf("Enter your name: ");
+	fgets(name, 255, stdin);
+	printf("length = %d\n", strlen(name)); /* debug line */
+	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+
+	printf("Hello %s!\n", name);
+	
 	return 0;
 }
