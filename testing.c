fce44456eff27aeb013d196357717e58af7258f4
TeamJAJA <jimxie@ucdavis.edu>
2011-04-09 15:38:23 -0700

    Use proper grammar.

--- testing.c
+++ testing.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 	printf("length = %d\n", strlen(name)); /* debug line */
 	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
-	printf("Hello %s!\n", name);
+	printf("Hello, %s!\n", name);
 	
 	return 0;
 }
